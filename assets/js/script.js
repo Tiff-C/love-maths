90e5a268b3e5d1c7de081de4f9c57a5e65d1e26d
Tiff-C <deguntiffany@gmail.com>
2021-07-29 16:03:52 +0000

    added code to  display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,17 +9,31 @@ document.addEventListener('DOMContentLoaded', function() {
           alert('You clicked Submit!');
         } else {
           let gameType = this.getAttribute('data-type');
-          alert(`You clicked ${gameType}`);
+          runGame(gameType);
         }
       })
     }
+
+    runGame('addition')
 })
 
-function runGame() {
+/**
+ * The main game 'loop' called when the script is first loaded
+ * and after the users answer has been processed.
+ */
+function runGame(gameType) {
 
   /* Creates random numbers between 1-25. The '+1' at the end is because Math.floor rounds down so would put out 0-24 not 1-25 */
   let num1 = Math.floor(Math.random() * 25) + 1;
   let num2 = Math.floor(Math.random() * 25) + 1;
+
+  if (gameType === 'addition') {
+    displayAdditionQuestion(num1, num2);
+  } else {
+    alert(`Unknown game type: ${gameType}`);
+    throw `Unknown game type: ${gameType}. Aborting!`
+  }
+
 }
 
 function checkAnswer() {
@@ -38,8 +52,11 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
-
+function displayAdditionQuestion(operand1, operand2) {
+  
+  document.getElementById('operand1').textContent = operand1;
+  document.getElementById('operand2').textContent = operand2;
+  document.getElementById('operator').textContent = '+';
 }
 
 function displaySubtractQuestion() {
