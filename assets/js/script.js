4ceab8c42a34e0223652579e16dc40bb4174d971
Tiff-C <deguntiffany@gmail.com>
2021-07-29 15:19:48 +0000

    add event listeners for document loading and button clicks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,21 @@
+/* always want to wait for DOM to finish loading before executing any code 
+  to ensure any elements you are targeting have finished loading */
+
+document.addEventListener('DOMContentLoaded', function() {
+    let buttons = document.getElementsByTagName('button');
+
+    for (let button of buttons) {
+      button.addEventListener('click', function() {
+        if (this.getAttribute('data-type') === 'submit') {
+          alert('You clicked Submit!');
+        } else {
+          let gameType = this.getAttribute('data-type');
+          alert(`You clicked ${gameType}`);
+        }
+      })
+    }
+})
+
 function runGame() {
 
 }
